8eb6c0afa7ab0b1cc9ad73b991cf3e3a1506d276
Jim <jimdab@mac.com>
2021-09-04 15:25:04 -0700

    Initial Commit

---
+++ All 50 Plates/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  All 50 Plates
+//
+//  Created by Jim Dabrowski on 9/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
