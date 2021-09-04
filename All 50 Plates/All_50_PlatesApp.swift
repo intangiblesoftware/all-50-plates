de70955442b85f38b63580a10c35995d7548df48
Jim <jimdab@mac.com>
2021-09-04 15:25:04 -0700

    Initial Commit

---
+++ All 50 Plates/All_50_PlatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_50_PlatesApp.swift
+//  All 50 Plates
+//
+//  Created by Jim Dabrowski on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct All_50_PlatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
