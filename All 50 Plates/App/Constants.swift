a2062319409d1d2e23d40b5b758c01d92233f039
Jim <jimdab@mac.com>
2021-09-20 10:38:19 -0700

    Added constants file and used them

---
+++ All 50 Plates/App/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  All 50 Plates
+//
+//  Created by Jim Dabrowski on 9/20/21.
+//
+
+// KeyConstants.swift
+struct Key {
+    struct UserDefaults {
+        static let listIsFiltered = "listIsFiltered"
+    }
+    
+    struct ResourceName {
+        static let statesPlist = "states.plist"
+    }
+}
+
